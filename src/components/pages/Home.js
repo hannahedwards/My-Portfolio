937b329506670f41dfe8002505bc138f054b5840
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-04-08 22:14:14 -0400

    changed file structure and tab titles

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Home() {
+export default function aboutMe() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Hannah Edwards</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
         velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
