029046dc98ea3de31c2c91df9e2599b0ca395adf
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-04-08 22:39:55 -0400

    added image

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import me from "../../images"
 export default function aboutMe() {
   return (
     <div>
-      <h1>Hannah Edwards</h1>
+      <section>
+      <img src="{me}" alt="me" />     
+       </section>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
         velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
